24d2f9d4260d37dd0295b48a6e8c241f53348af5
Robin Thomas <robinthomas2591@gmail.com>
2019-06-29 12:22:01 +0800

    create Piper smart contract

--- contracts/Piper.sol
+++ contracts/Piper.sol
@@ -1,18 +1,58 @@
 pragma solidity ^0.5.0;
+pragma experimental ABIEncoderV2;
 
 contract Piper {
-  string saySomething;
+  address owner;
+  string name;
 
-  constructor() public  {
-    saySomething = "Hello World!";
+  struct Extension {
+    address owner;
+    string hash;
+    string developer;
+    address developerETH;
+    string name;
+    string overview;
+    string category;
+    string version;
+    uint extensionSize;
+    string iconURL;
+    string extensionCrxURL;
+    uint8 rating;
+    uint reviews;
+    uint downloads;
+    uint updated;
+  }
+  mapping(string => Extension) extensions;
+
+  constructor() public {
+    owner = msg.sender;
+    name = "Piper";
   }
 
-  function speak() public view returns(string memory) {
-    return saySomething;
+  function getExtensionByHash(string memory hash) public view returns(Extension memory) {
+    // Make sure the extension exists.
+    require(keccak256(bytes(extensions[hash].hash)) == keccak256(bytes(hash)));
+
+    return extensions[hash];
   }
 
-  function saySomethingElse(string memory newSaying) public returns(bool) {
-    saySomething = newSaying;
+  function createNewExtension(string memory hash, Extension memory extension) public returns(bool) {
+    // Make sure that the extension doesnt exist.
+    require(keccak256(bytes(extensions[hash].hash)) != keccak256(bytes(hash)));
+
+    // Create new extension.
+    extensions[hash] = extension;
+
+    return true;
+  }
+
+  function updateExtensionByHash(string memory hash, Extension memory extension) public returns(bool) {
+    // Check if extension owner is the one trying to edit it.
+    require(msg.sender == extensions[hash].owner);
+
+    // Update the extension.
+    extensions[hash] = extension;
+
     return true;
   }
 }
