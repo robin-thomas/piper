03e09a40601214b9cc8f87b137a489e8635e50df
Robin Thomas <robinthomas2591@gmail.com>
2019-06-30 11:04:59 +0800

    Improve the header

--- components/Header.js
+++ components/Header.js
@@ -5,13 +5,29 @@ import { Container, Row, Col } from "react-bootstrap";
 const Header = () => (
   <Container fluid="true">
     <Row className="header-container">
-      <Col className="align-self-center text-center"><img src="/static/images/chrome.svg" />chrome web store</Col>
+      <Col className="align-self-center">
+        <Container>
+          <Row>
+            <Col>
+              <img src="/static/images/chrome.svg" />
+              <span>chrome web store</span>
+            </Col>
+            <Col className="align-self-center sign-in text-right">
+              <img
+                src="/static/images/settings.svg"
+                style={{ width: "20px" }}
+              />
+              <span>Sign in</span>
+            </Col>
+          </Row>
+        </Container>
+      </Col>
     </Row>
     <style jsx global>{`
       .header-container {
         background: #fff;
         height: 64px;
-        box-shadow: 0 2px 6px 0 rgba(0,0,0,0.12);
+        box-shadow: 0 2px 6px 0 rgba(0, 0, 0, 0.12);
         font-size: 1.375rem;
         font-weight: 400;
         line-height: 1.75rem;
@@ -22,6 +38,15 @@ const Header = () => (
       .header-container img {
         width: 30px;
         margin-right: 10px;
+      }
+
+      .sign-in {
+        color: #5f6368;
+        cursor: pointer;
+        letter-spacing: 0.01428571em;
+        font-size: 0.875rem;
+        font-weight: 400;
+        line-height: 1.25rem;
       }
     `}</style>
   </Container>
