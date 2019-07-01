6a946d75bb80864a51e2cb7d4177f09cf7d928fd
Robin Thomas <robinthomas2591@gmail.com>
2019-07-01 13:32:59 +0800

    Improve UI while creating new extension

--- components/Content.js
+++ components/Content.js
@@ -1,27 +1,52 @@
 import Link from "next/link";
 
-import { Row, Col, Button } from "react-bootstrap";
+import { useState } from "react";
+
+import { Row, Col, Button, Spinner } from "react-bootstrap";
 
 import { DataConsumer } from "./utils/DataProvider";
 
-const Content = props => (
-  <Row>
-    <DataConsumer>
-      {context => (
-        <Col md="auto" className="ml-auto">
-          {context.loggedIn === true ? (
-            <Link href="extensions?hash=new" as="extensions/new">
-              <a>
-                <Button variant="success" disabled={!context.loggedIn}>
-                  Add New Extension
-                </Button>
-              </a>
-            </Link>
-          ) : null}
-        </Col>
-      )}
-    </DataConsumer>
-  </Row>
-);
+const Content = props => {
+  const [disableAddNewButton_, disableAddNewButton] = useState(false);
+
+  const save = e => {
+    disableAddNewButton(true);
+  };
+
+  return (
+    <Row>
+      <DataConsumer>
+        {context => (
+          <Col md="auto" className="ml-auto">
+            {context.loggedIn === true ? (
+              <Link href="extensions?hash=new" as="extensions/new">
+                <a>
+                  <Button
+                    variant="success"
+                    onClick={save}
+                    disabled={disableAddNewButton_}
+                  >
+                    <Spinner
+                      animation={`${disableAddNewButton_ ? "border" : null}`}
+                      size="sm"
+                      role="status"
+                    />
+                    <span
+                      style={{
+                        display: `${disableAddNewButton_ ? "none" : "inline"}`
+                      }}
+                    >
+                      Add New Extension
+                    </span>
+                  </Button>
+                </a>
+              </Link>
+            ) : null}
+          </Col>
+        )}
+      </DataConsumer>
+    </Row>
+  );
+};
 
 export default Content;
