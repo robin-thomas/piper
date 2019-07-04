490c9023872dcfa86f3b4a7048b27f438c57bd04
Robin Thomas <robinthomas2591@gmail.com>
2019-07-04 17:17:25 +0800

    Add support for various verions in contract

--- contracts/Piper.sol
+++ contracts/Piper.sol
@@ -30,6 +30,14 @@ contract Piper {
   mapping(string => ExtensionWithOwner) extensions;
   mapping(string => bool) hasExtension;
 
+  struct ExtensionVersion {
+    string version;
+    string crx;
+    uint created;
+  }
+
+  mapping(string => ExtensionVersion[]) versions;
+
   constructor() public {
     owner = msg.sender;
     name = "Piper";
@@ -48,6 +56,7 @@ contract Piper {
 
   event CreateNewExtensionEvent(string hash, ExtensionWithOwner extension);
   event UpdateExtensionByHashEvent(string hash, ExtensionWithOwner extension);
+  event CreateNewExtensionVersionEvent(string hash, ExtensionVersion version);
 
   function createNewExtension(string memory _hash, Extension memory _extension) public returns(bool) {
     // Make sure that the extension doesnt exist.
@@ -68,8 +77,36 @@ contract Piper {
     require(isExtension(_hash));
     require(msg.sender == extensions[_hash].owner);
 
+    // Check whether they are updating the version.
+    if (keccak256(bytes(_extension.crx)) != keccak256(bytes(extensions[_hash].extension.crx))) {
+      ExtensionVersion memory _extensionVersion = ExtensionVersion(_extension.version, _extension.crx, _extension.updated);
+      versions[_hash].push(_extensionVersion);
+
+      extensions[_hash].extension.developer = _extension.developer;
+      extensions[_hash].extension.developerETH = _extension.developerETH;
+      extensions[_hash].extension.name = _extension.name;
+      extensions[_hash].extension.overview = _extension.overview;
+      extensions[_hash].extension.category = _extension.category;
+      extensions[_hash].extension.size = _extension.size;
+      extensions[_hash].extension.iconURL = _extension.iconURL;
+      extensions[_hash].extension.updated = _extension.updated;
+
+      emit CreateNewExtensionVersionEvent(_hash, _extensionVersion);
+
+      return true;
+    }
+
     // Update the extension.
-    extensions[_hash].extension = _extension;
+    extensions[_hash].extension.developer = _extension.developer;
+    extensions[_hash].extension.developerETH = _extension.developerETH;
+    extensions[_hash].extension.name = _extension.name;
+    extensions[_hash].extension.overview = _extension.overview;
+    extensions[_hash].extension.category = _extension.category;
+    extensions[_hash].extension.size = _extension.size;
+    extensions[_hash].extension.iconURL = _extension.iconURL;
+    extensions[_hash].extension.updated = _extension.updated;
+    extensions[_hash].extension.version = _extension.version;
+    extensions[_hash].extension.crx = _extension.crx;
 
     emit UpdateExtensionByHashEvent(_hash, extensions[_hash]);
 
