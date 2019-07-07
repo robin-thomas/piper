b47fe408ab998301860bb82f9c5fd66e942f01b5
Robin Thomas <robinthomas2591@gmail.com>
2019-07-08 07:35:21 +0800

    ui for adding a review/rating

--- components/extension/ExtensionReviews.js
+++ components/extension/ExtensionReviews.js
@@ -1,8 +1,20 @@
-import { Container, Row, Col } from "react-bootstrap";
+import { useState } from "react";
 
+import { MDBInput } from "mdbreact";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import StarRatings from "react-star-ratings";
+
+import { DataConsumer } from "../utils/DataProvider";
 import EmptyRow from "../utils/EmptyRow";
 
 const ExtensionReviews = props => {
+  const [rating, setRating] = useState(0);
+  const [review, setReview] = useState(null);
+
+  const changeRating = newRating => {
+    setRating(newRating);
+  };
+
   return (
     <Container>
       <EmptyRow cls="extension-details-empty-row" />
@@ -19,6 +31,46 @@ const ExtensionReviews = props => {
               </Col>
             </Row>
           ) : null}
+          <EmptyRow />
+          <EmptyRow />
+          <EmptyRow />
+          <Row>
+            <Col className="align-self-center">
+              <Button
+                variant="outline-dark"
+                disabled={
+                  review !== null && review.length > 0 && rating > 0
+                    ? false
+                    : true
+                }
+              >
+                Submit Review
+              </Button>
+            </Col>
+            <Col className="align-self-center ml-auto" md="auto">
+              <StarRatings
+                rating={rating}
+                starRatedColor="black"
+                changeRating={changeRating}
+                numberOfStars={5}
+                name="rating"
+                starDimension="25px"
+                starSpacing="5px"
+              />
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <MDBInput
+                type="textarea"
+                hint="Add your review"
+                rows="5"
+                value={review !== null ? review : ""}
+                onChange={e => setReview(e.target.value)}
+                disabled={false}
+              />
+            </Col>
+          </Row>
         </Col>
       </Row>
       <style jsx global>{`
